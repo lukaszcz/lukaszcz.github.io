1765a4832dcdcd148c4d3b2542a49b7a4cc3ed87
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2025-05-24 20:16:59 +0200

    sticky

--- assets/js/head-sticky-toc.js
+++ assets/js/head-sticky-toc.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const toc = document.querySelector('.sidebar__right .toc');
-  if (toc) {
-    Object.assign(toc.style, {
-      position: 'sticky',
-      top: '2rem',
-      maxHeight: 'calc(100vh - 4rem)',
-      overflowY: 'auto'
-    });
-  }
+  const aside = document.querySelector(".sidebar__right");
+  if (!aside) return;
+
+  Object.assign(aside.style, {
+    position: "sticky",
+    top: "2rem",
+    maxHeight: "calc(100vh - 4rem)",
+    overflowY: "auto",
+    zIndex: "1"
+  });
 });
